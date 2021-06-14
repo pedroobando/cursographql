56c09396c1b76ec9a2e4ca34cc189f4578f5ca32
pedroobando <pedroobando@hotmail.com>
2021-06-14 15:35:11 -0400

    holamundo graphql

---
+++ 01_holamundo/src/server.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import compression from 'compression';
+import cors from 'cors';
+import { ApolloServer } from 'apollo-server-express';
+// import { graphqlHTTP } from "express-graphql";
+
+import schema from './schema/index';
+import { createServer } from 'http';
+
+const app = express();
+const PORT = 5300;
+
+app.use(cors());
+app.use(compression());
+
+const server = new ApolloServer({
+  schema,
+  introspection: true,
+});
+
+server.applyMiddleware({ app });
+// app.use(
+//   "/graphql",
+//   graphqlHTTP({
+//     schema,
+//     graphiql: true,
+//   })
+// );
+const httpServer = createServer(app);
+
+httpServer.listen({ port: PORT }, () => {
+  console.log(`Hola Mundo API GraphQL http://localhost:${PORT}/graphql`);
+});
