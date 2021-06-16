ee89ae1d21758363d1740572c89d677d78bf85db
pedroobando <pedroobando@hotmail.com>
2021-06-15 23:59:19 -0400

    mutation

--- 02academiaonline/src/resolvers/mutation.ts
+++ 02academiaonline/src/resolvers/mutation.ts
@@ -1,6 +1,25 @@
 import { IResolvers } from 'graphql-tools';
 import { database } from '../data/data.store';
 
-const mutation: IResolvers = {};
+const mutation: IResolvers = {
+  Mutation: {
+    cursoNuevo(__: void, { curso }): any {
+      const itemCurso = {
+        id: String(database.cursos.length + 1),
+        title: curso.title,
+        description: curso.description,
+        clases: curso.clases,
+        time: curso.time,
+        logo: curso.logo,
+        level: curso.level,
+        path: curso.path,
+        teacher: curso.teacher,
+        reviews: [],
+      };
+      database.cursos.push(itemCurso);
+      return itemCurso;
+    },
+  },
+};
 
 export default mutation;
