7fe997514f269f4da27930eda2566cba20edd87c
pedroobando <pedroobando@hotmail.com>
2021-06-15 22:07:17 -0400

    cursos

--- 02academiaonline/src/resolvers/query.ts
+++ 02academiaonline/src/resolvers/query.ts
@@ -6,6 +6,42 @@ const query: IResolvers = {
     estudiantes(): any {
       return database.estudiantes;
     },
+    estudiante(__: void, { id }): any {
+      const resultado = database.estudiantes.find((estudiante) => estudiante.id === id);
+      if (typeof resultado === 'undefined') {
+        return {
+          id: '-1',
+          name: `Estudiante no encontrado con el id: ${id}`,
+          email: '',
+          website: '',
+          courses: [],
+        };
+      }
+      return resultado;
+    },
+
+    cursos(): any {
+      return database.cursos;
+    },
+    curso(__: void, { id }): any {
+      const resultado = database.cursos.find((elCurso) => elCurso.id === id);
+      if (typeof resultado === 'undefined') {
+        return {
+          id: '-1',
+          title: `No se encontro el curso con el id:${id}`,
+          description: '',
+          clases: 0,
+          time: 0,
+          logo: '',
+          level: '',
+          path: '',
+          teacher: '',
+          students: [],
+          reviews: []!,
+        };
+      }
+      return resultado;
+    },
   },
 };
 
