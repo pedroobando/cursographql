149858a1db8ba48da387ec2d715d798f8f36c1d8
pedroobando <pedroobando@hotmail.com>
2021-06-18 06:18:01 -0400

    problems type null

--- 03resttoapigraphql/src/resolvers/type.ts
+++ 03resttoapigraphql/src/resolvers/type.ts
@@ -6,6 +6,18 @@ const type: IResolvers = {
     year: (parent) => parent.season,
     urlMobile: (parent) => getWikipediaMobileUrl(parent.url),
   },
+  Race: {
+    name: (parent) => parent.raceName,
+    circuit: (parent) => parent.Circuit,
+  },
+  Circuit: {
+    id: (parent) => parent.circuitId,
+    name: (parent) => parent.circuitName,
+    location: (parent) => parent.Location,
+  },
+  Location: {
+    lng: (parent) => parent.long,
+  },
 };
 
 export default type;
